d2219d059a4a4af1acc3ece6f2e236a92a517346
Krystian Szabat <krystian.szabat@outlook.com>
2024-04-16 17:07:55 +0200

    wnioski na szybko

--- lab_7/main.py
+++ lab_7/main.py
@@ -34,8 +34,20 @@ def quantize(signal, bits):
     return data
 
 
+# zmienic
+# def quantize(data, bit):
+#     res = data
+#     start = -1
+#     end = 1
+#     q_range = 2**bit - 1
+
+#     res = (res - start) / (end - start)
+#     res = np.round(res * q_range) / q_range
+#     res = (res * (end - start)) + start
+#     return res
+
+
 def muLaw_encode(data, mu=255):
-    # data = data.astype(float)
     data = data.copy()
     data = np.sign(data) * np.log(1 + mu * np.abs(data)) / np.log(1 + mu)
 
@@ -44,8 +56,7 @@ def muLaw_encode(data, mu=255):
 
 def muLaw_decode(data, mu=255):
     data = data.copy()
-    # data = data.astype(float)
-    data = np.sign(data) * (1 / mu) * (np.power(1 + mu, np.abs(data) - 1))
+    data = np.sign(data) * (1 / mu) * (np.power(1 + mu, np.abs(data)) - 1)
 
     return data
 
@@ -66,6 +77,7 @@ def DPCM_predict_encode(data, bit, predictor, n):
 
     return y
 
+
 def DPCM_predict_decode(data, predictor, n):
     y = data.copy()
     xp = data.copy()
@@ -85,53 +97,171 @@ def DPCM_predict_decode(data, predictor, n):
 
 def predictor(X):
     return np.median(X)
+    # return np.mean(X)
     # return X[-1]
 
 
-    
+def DPCM_predict(data, bit, predictor, n):
+    y = DPCM_predict_encode(data, bit, predictor, n)
+    y = DPCM_predict_decode(y, predictor, n)
+    return y
 
-if __name__ == "__main__":
+
+def MuLaw(data, bits=6, mu=255):
+    data = muLaw_encode(data, mu)
+    data = quantize(data, bits)
+    data = muLaw_decode(data, mu)
+    return data
+
+
+def draw_plot(titles, x, y, xlim=(-1, 1), margin=0.1):
+    fig, axs = plt.subplots(len(titles), 1, figsize=(15, 10))
+
+    for i, title in enumerate(titles):
+        axs[i].plot(x, y[i])
+        axs[i].set_title(title)
+
+    for ax in axs.flatten():
+        ax.set_xlim([xlim[0], xlim[1]])
+        current_xlim = ax.get_xlim()
+        new_xlim = (current_xlim[0] - margin, current_xlim[1] + margin)
+        ax.set_xlim(new_xlim)
+
+    return fig
+
+
+def save_plot(fig, filename):
+    fig.savefig(filename, dpi=600)
+
+
+def main_comparison():
     x = np.linspace(-1, 1, 1000)
     y = 0.9 * np.sin(np.pi * x * 4)
 
-    y_muLaw_encoded = muLaw_encode(y)
-    y_quantized = quantize(y_muLaw_encoded, 6)
-    y_muLaw_decoded = muLaw_decode(y_quantized)
+    bits = range(2, 9)
+    margin = 0.1
+    for _bits in bits:
+        y_muLaw_compressed = MuLaw(y, _bits)
+        y_dpcm_compressed = DPCM_predict(y, _bits, predictor, 6)
 
-    fig, axs = plt.subplots(3, 1, figsize=(10, 10))
-    axs[0].plot(x, y)
-    axs[0].set_title("Original Signal")
-    axs[1].plot(x, y_muLaw_encoded)
-    axs[1].set_title("Mu-Law Encoded Signal")
-    axs[2].plot(x, y_muLaw_decoded)
-    axs[2].set_title("Mu-Law Decoded Signal")
-    axs[0].plot(x, y)
-    axs[0].set_title("Original Signal")
-    axs[1].plot(x, y_muLaw_encoded)
-    axs[1].set_title("Mu-Law Encoded Signal")
-    axs[2].plot(x, y_muLaw_decoded)
-    axs[2].set_title("Mu-Law Decoded Signal")
+        fig = draw_plot(
+            [
+                "Original Signal",
+                "Mu-Law compressed Signal",
+                "DPCM (predict) compressed  Signal",
+            ],
+            x,
+            [y, y_muLaw_compressed, y_dpcm_compressed],
+            xlim=[-1, 1],
+            margin=margin,
+        )
 
-    print(np.max(y), np.min(y), np.max(y_muLaw_decoded), np.min(y_muLaw_decoded))
+        # plt.show()
+        save_plot(fig, f"OUTPUT/plot_{_bits}bits.png")
 
-    for ax in axs.flatten():
-        ax.set_xlim([0, 1])
+        fig, ax = plt.subplots(1, 1, figsize=(15, 5))
+        ax.plot(x, y, label="Original Signal")
+        ax.plot(x, y_muLaw_compressed, label="Mu-Law compressed Signal")
+        ax.plot(x, y_dpcm_compressed, label="DPCM (predict) compressed  Signal")
+        ax.legend()
+        ax.set_xlim([-1, 1])
+        current_xlim = ax.get_xlim()
+        new_xlim = (current_xlim[0] - margin, current_xlim[1] + margin)
+        ax.set_xlim(new_xlim)
+        save_plot(fig, f"OUTPUT/x_plot_{_bits}bits.png")
 
-    plt.show()
+
+def main_sing():
+    file, fs = sf.read("SING/sing_high1.wav")
+
+    bits = 2
+    n = 6
+
+    filem_muLaw = MuLaw(file, bits)
+    filem_DPCM = DPCM_predict(file, bits, predictor, n)
+
+    # sd.play(file, fs)
+    # sd.wait()
+
+    sd.play(filem_muLaw, fs)
+    sd.wait()
+
+    sd.play(filem_DPCM, fs)
+    sd.wait()
 
 
-    y_DPCM_encoded = DPCM_predict_encode(y, 6, predictor, 3)
-    y_DPCM_decoded = DPCM_predict_decode(y_DPCM_encoded, predictor, 3)
+if __name__ == "__main__":
+    # main_comparison()
+    main_sing()
 
-    fig, axs = plt.subplots(3, 1, figsize=(10, 10))
-    axs[0].plot(x, y)
-    axs[0].set_title("Original Signal")
-    axs[1].plot(x, y_DPCM_encoded)
-    axs[1].set_title("DPCM Encoded Signal")
-    axs[2].plot(x, y_DPCM_decoded)
-    axs[2].set_title("DPCM Decoded Signal")
+    # x = np.linspace(-1, 1, 1000)
+    # y = 0.9 * np.sin(np.pi * x * 4)
 
-    for ax in axs.flatten():
-        ax.set_xlim([0, 1])
+    # y_muLaw_encoded = muLaw_encode(y)
+    # y_quantized = quantize(y_muLaw_encoded, 6)
+    # y_muLaw_decoded = muLaw_decode(y_quantized)
 
-    plt.show()
+    # fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+    # axs[0].plot(x, y)
+    # axs[0].set_title("Original Signal")
+    # axs[1].plot(x, y_muLaw_encoded)
+    # axs[1].set_title("Mu-Law Encoded Signal")
+    # axs[2].plot(x, y_muLaw_decoded)
+    # axs[2].set_title("Mu-Law Decoded Signal")
+    # axs[0].plot(x, y)
+    # axs[0].set_title("Original Signal")
+    # axs[1].plot(x, y_muLaw_encoded)
+    # axs[1].set_title("Mu-Law Encoded Signal")
+    # axs[2].plot(x, y_muLaw_decoded)
+    # axs[2].set_title("Mu-Law Decoded Signal")
+
+    # print(np.max(y), np.min(y), np.max(y_muLaw_decoded), np.min(y_muLaw_decoded))
+
+    # for ax in axs.flatten():
+    #     ax.set_xlim([0, 1])
+
+    # plt.show()
+
+    # y_DPCM_encoded = DPCM_predict_encode(y, 6, predictor, 3)
+    # y_DPCM_decoded = DPCM_predict_decode(y_DPCM_encoded, predictor, 3)
+
+    # fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+    # axs[0].plot(x, y)
+    # axs[0].set_title("Original Signal")
+    # axs[1].plot(x, y_DPCM_encoded)
+    # axs[1].set_title("DPCM Encoded Signal")
+    # axs[2].plot(x, y_DPCM_decoded)
+    # axs[2].set_title("DPCM Decoded Signal")
+
+    # for ax in axs.flatten():
+    #     ax.set_xlim([0, 1])
+
+    # def encode_DPCM(data, bit):
+    #     res = data.copy()
+    #     E = res[0]
+    #     for x in range(1, data.shape[0]):
+    #         diff = data[x] - E
+    #         diff = quantize(diff, 16)
+    #         res[x] = diff
+    #         E += diff
+    #     return res
+
+    # def decode_DPCM(data):
+    #     res = data.copy()
+    #     for x in range(1, data.shape[0]):
+    #         res[x] = res[x - 1] + data[x]
+    #     return res
+
+    # def DPCM_compression(data, bit):
+    #     res = encode_DPCM(data, bit)
+    #     res = decode_DPCM(res)
+    #     return res
+
+    # X = np.array([15,16,20,14,5,10,15,13,11,7,10,11,20,1,23])
+
+    # y = DPCM_predict(X, 7, predictor, 3)
+    # print(y)
+    # y = DPCM_predict_decode(y, predictor, 3)
+    # print(y)
+
+    # plt.show()
