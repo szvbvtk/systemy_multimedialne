af75a0a8f22fcae14219510d104628a70db79d99
Krystian Szabat <krystian.szabat@outlook.com>
2024-06-03 12:24:10 +0200

    12, czesciowo zrobione

--- lab_11/main.py
+++ lab_11/main.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 from skimage.metrics import structural_similarity as ssim
 
 
-def SSIM(source_image, target_image):
-    return ssim(source_image, target_image, channel_axis=2)
+def IF(source_image, target_image):
+    return 1 - (np.sum((source_image - target_image) ** 2)) / np.sum(
+        np.multiply(source_image, target_image)
+    )
 
 
 def MSE(source_image, target_image):
@@ -99,107 +101,137 @@ def read_image(image_path):
     return image
 
 
+def main_watermark():
+    img_dir = Path("./img")
+    img1_path = img_dir / "5.jpg"
+    mask_path = img_dir / "watermark2.jpg"
+
+    img1 = read_image(img1_path)
+    mask = read_image(mask_path)
+
+    img1 = cv2.resize(img1, (mask.shape[1], mask.shape[0]))
+
+    # watermarked = watermark(img1, mask, alpha=0.5)
+
+    # fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    # axs[0].imshow(img1)
+    # axs[0].set_title("Original image")
+    # axs[0].axis("off")
+    # axs[1].imshow(mask)
+    # axs[1].set_title("Mask")
+    # axs[1].axis("off")
+    # axs[2].imshow(watermarked)
+    # axs[2].set_title("Watermarked image")
+    # axs[2].axis("off")
+    # plt.show()
+
+    # mse = round(MSE(img1, watermarked), 2)
+    # if_ = round(IF(img1, watermarked), 2)
+
+    # print(f"MSE: {mse}")
+    # print(f"IF: {if_}")
+
+    MSE_ = []
+    IF_ = []
+
+    for i in range(5, 95, 10):
+        watermarked = watermark(img1, mask, alpha=i / 100)
+        mse = round(MSE(img1, watermarked), 2)
+        if_ = round(IF(img1, watermarked), 2)
+        MSE_.append(mse)
+        IF_.append(if_)
+
+        plt.imshow(watermarked)
+        plt.title(f"Watermarked image with alpha = {i}%")
+        plt.axis("off")
+        plt.savefig(f"output_watermark/{i}.png")
+        plt.close()
+
+    plt.plot(range(5, 85, 10), MSE_, label="MSE")
+    plt.xlabel("Alpha [%]")
+    plt.ylabel("Value")
+    plt.legend()
+    plt.savefig("output_watermark/metric1.png")
+    plt.close()
+    plt.plot(range(5, 85, 10), IF_, label="IF")
+    plt.xlabel("Alpha [%]")
+    plt.ylabel("Value")
+    plt.legend()
+    plt.savefig("output_watermark/metric2.png")
+
+
 def main():
     img_dir = Path("./img")
-    img_path = img_dir / "1.png"
-    img2_path = img_dir / "2.jpg"
-    watermark_path = img_dir / "2.jpg"
+    img1_path = img_dir / "1.jpg"
+    img2_path = img_dir / "watermark2.jpg"
 
-    # img = read_image(img_path)
-    # # mask = read_image(img2_path)
-    # # cv2.resize(mask, (img.shape[1], img.shape[0]))
-
-    # img2 = read_image(watermark_path)
-
-    # # print(img.shape, mask.shape)
-    # # print(img.dtype, mask.dtype)
-    # # watermarked_img = watermark(img, mask, 0.4)
-
-    # # mask = np.random.randint(0, 2, (img.shape[0], img.shape[1]), dtype=bool)
-    # # alpha = 0.25
-    # # watermarked_img = watermark(img, mask, alpha)
-
-    # # fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-    # # ax[0].imshow(img)
-    # # ax[0].set_title("Original image")
-    # # ax[0].axis("off")
-    # # ax[1].imshow(mask, cmap="gray")
-    # # ax[1].set_title("Mask")
-    # # ax[1].axis("off")
-    # # ax[2].imshow(watermarked_img)
-    # # ax[2].set_title("Watermarked image")
-    # # ax[2].axis("off")
-    # # plt.show()
-
-    # img2 = img2[200 : img.shape[0] // 2, 100 : img.shape[1] // 2]
-    # shape = img2.shape
-    # img2 = img2.flatten()
-    # img2 = img2.reshape(shape)
-    # plt.imshow(img2)
-    # plt.show()
-    # # print(img2.flatten().shape)
-
-    img = read_image(img_path)
+    img1 = read_image(img1_path)
     img2 = read_image(img2_path)
-    img2 = img2[200 : img.shape[0] // 2, 100 : img.shape[1] // 2]
-    shape = img2.shape
+
+    print(min(img1.flatten()), max(img1.flatten()))
+    print(min(img2.flatten()), max(img2.flatten()))
+
+    img2_shape = img2.shape
     img2 = img2.flatten()
     l = len(img2) // 3
     img2_1 = img2[:l]
     img2_2 = img2[l : 2 * l]
     img2_3 = img2[2 * l :]
 
-    img_r = put_data(img[:, :, 0], img2_1, np.uint8(12))
-    img_g = put_data(img[:, :, 1], img2_2, np.uint8(12))
-    img_b = put_data(img[:, :, 2], img2_3, np.uint8(12))
+    binary_mask1 = 1
+    binary_mask2 = 1
+    binary_mask3 = 1
 
-    r = pop_data(img_r, np.uint8(12), out_shape=img2_1.shape)
-    g = pop_data(img_g, np.uint8(12), out_shape=img2_2.shape)
-    b = pop_data(img_b, np.uint8(12), out_shape=img2_3.shape)
+    img_r = put_data(img1[:, :, 0], img2_1, np.uint8(binary_mask1))
+    img_g = put_data(img1[:, :, 1], img2_2, np.uint8(binary_mask2))
+    img_b = put_data(img1[:, :, 2], img2_3, np.uint8(binary_mask3))
+
+    r = pop_data(img_r, np.uint8(binary_mask1), out_shape=img2_1.shape)
+    g = pop_data(img_g, np.uint8(binary_mask2), out_shape=img2_2.shape)
+    b = pop_data(img_b, np.uint8(binary_mask3), out_shape=img2_3.shape)
 
     vector = np.concatenate((r.flatten(), g.flatten(), b.flatten()))
 
-    new_img2 = vector.reshape(shape)
-    plt.imshow(new_img2)
-    plt.show()
-
-    new_img1 = np.empty_like(img)
+    new_img1 = np.empty_like(img1)
     new_img1[:, :, 0] = img_r
     new_img1[:, :, 1] = img_g
     new_img1[:, :, 2] = img_b
 
-    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
-    axs[0].imshow(img)
+    fig, axs = plt.subplots(1, 2, figsize=(15, 10))
+    axs[0].imshow(img1)
     axs[0].set_title("Original image")
     axs[0].axis("off")
     axs[1].imshow(new_img1)
     axs[1].set_title("with hidden data")
     axs[1].axis("off")
-    plt.show()
-    
-
-    # mask = np.array(
-    #     [
-    #         ord(c)
-    #         for c in "Hello, World!Hello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, WorldHello, World"
-    #     ],
-    #     dtype=np.uint8,
-    # )
-    # # mask = np.unpackbits(mask)
-    # print(mask)
-    # img_r = put_data(img[:, :, 0], mask, np.uint8(1))
-    # r = pop_data(img_r, np.uint8(1), out_shape=(1, len(mask)))
-    # print(r)
-    # # print(r)
-    # # print(mask[:200, :200, 0] - r)
-    # # # print(r[:10])
-    # img[:, :, 0] = img_r
-    # plt.imshow(img)
+    plt.tight_layout()
     # plt.show()
+    plt.savefig(f"output_steg/1_{binary_mask1}_{binary_mask2}_{binary_mask3}.png")
+
+    img2_reconstructed = vector.reshape(img2_shape)
+    plt.imshow(img2_reconstructed)
+    plt.show()
+
+    # Miary jakosci
+    mse_r = round(MSE(img1[:, :, 0], img_r), 2)
+    mse_g = round(MSE(img1[:, :, 1], img_g), 2)
+    mse_b = round(MSE(img1[:, :, 2], img_b), 2)
+
+    IF_r = round(IF(img1[:, :, 0], img_r), 2)
+    IF_g = round(IF(img1[:, :, 1], img_g), 2)
+    IF_b = round(IF(img1[:, :, 2], img_b), 2)
+
+    print(f"MSE R: {mse_r}")
+    print(f"MSE G: {mse_g}")
+    print(f"MSE B: {mse_b}")
+    print(f"IF R: {IF_r}")
+    print(f"IF G: {IF_g}")
+    print(f"IF B: {IF_b}")
 
 
 if __name__ == "__main__":
     main()
+    # main_watermark()
     # test()
 
     # text = "Hello, World!"
